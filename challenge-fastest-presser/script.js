a91ecc7d0d6aae0bd6b8846b0f912cd9d82a32d3
Mpanasetckiy <maxbuzz8694@gmail.com>
2023-03-12 23:50:46 +0000

    fastest presser done

--- challenge-fastest-presser/script.js
+++ challenge-fastest-presser/script.js
@@ -1,11 +1,65 @@
-function startGame() {}
+// Here are all of the elements we need for the game
+const input = document.querySelector('#timerInput');
+const startBtn = document.querySelector("#startButton")
+const playerOneScore = document.querySelector("#playerOneScore");
+const playerTwoScore = document.querySelector("#playerTwoScore");
+const winnerText = document.querySelector("#wonText");
+const incorrectInputText = document.querySelector("#incorrectInput")
+const countDownText = document.querySelector("#countDown");
 
+let gameIsOver = true; // Show that game is not running by default
+let sCounter = 0;
+let lCounter = 0;
+
+function startGame() {
+const inputValue = parseInt(input.value, 10);
+gameIsOver = false; // Set game running
+// This part repeats score to zero every time start btn is pressed
+sCounter = 0;
+lCounter = 0;
+playerOneScore.textContent = `Score: ${sCounter}`
+playerTwoScore.textContent = `Score: ${lCounter}`
+//
+if (!isNaN(inputValue) && inputValue > 0) {
+  incorrectInputText.textContent = ""; // Set incorrect input field empty
+ 
+  let countdown = inputValue;
+  const countdownInterval = setInterval(() => {
+    countdown--;
+    countDownText.textContent = `Time left ${countdown}`;
+    if (countdown === 0) {
+      clearInterval(countdownInterval);
+      endGame();
+    }
+  }, 1000);
+} else {
+  incorrectInputText.textContent = "Type number greater than 0" // Shows extra field that input incorrect
+}
+}
+
+function endGame() {
+  if (sCounter > lCounter) {
+    winnerText.textContent = "Player one won!"
+  } else if (lCounter > sCounter) {
+    winnerText.textContent = "Player two won!"
+  } else if (lCounter === sCounter) {
+    winnerText.textContent = "Players tied!"
+  }
+  countDownText.textContent = `Time's up`;
+  gameIsOver = true;
+  console.log('Game over!');
+}
 function keyBoardEvents(e) {
-  if (e.keyCode === 83) {
+  if (e.key === 's' && !gameIsOver) {
     // On 'S' Pressed
-  } else if (e.keyCode === 76) {
+    sCounter++;
+    playerOneScore.textContent = `Score: ${sCounter}`
+  } else if (e.key === "l" && !gameIsOver) {
     // On 'L' Pressed
+    lCounter++;
+    playerTwoScore.textContent = `Score: ${lCounter}`
   }
 }
 
 document.addEventListener("keypress", keyBoardEvents);
+startBtn.addEventListener("click", startGame)
